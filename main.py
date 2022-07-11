e2d52bbb93e6c3dcedeb421ad76a9c13f5c84d65
Filip Lange <filip.lange@gmail.com>
2022-07-11 22:26:56 +0200

    Add mousewheel zoom

--- main.py
+++ main.py
@@ -9,12 +9,23 @@ GRID_BOX_SIZE = 30
 GRID_LINE_WIDTH = 1
 GRID_COLOR = arcade.color.BLACK
 
+# Min zoom = 1/MAX_CAMERA_SCALE, i.e. 25%
+MAX_CAMERA_SCALE = 4
+# Max zoom = 1/MIN_CAMERA_SCALE, i.e. 200%
+MIN_CAMERA_SCALE = 0.5
+
 
 class MyGame(arcade.Window):
 
     VISIBLE = True  # Overridden for unit testing
+
     def __init__(self):
-        super().__init__(width=SCREEN_WIDTH, height=SCREEN_HEIGHT, title="TRAINFINITY", visible=self.VISIBLE)
+        super().__init__(
+            width=SCREEN_WIDTH,
+            height=SCREEN_HEIGHT,
+            title="TRAINFINITY",
+            visible=self.VISIBLE,
+        )
 
         self.horizontal_grid_lines = []
         self.vertical_grid_lines = []
@@ -59,8 +70,25 @@ class MyGame(arcade.Window):
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
         if self.is_mouse2_pressed:
             delta = Vec2(x - self.mouse2_pressed_x, y - self.mouse2_pressed_y)
+
+            # Required for panning to work correctly when zoomed in or out
+            delta = delta.scale(self.camera_sprites.scale)
+
             new_position = self.camera_position_when_mouse2_pressed - delta
+
             self.camera_sprites.move(new_position)
+
+    def on_mouse_scroll(self, x: int, y: int, scroll_x: int, scroll_y: int):
+        if scroll_y < 0:
+            scale_delta = 0.1
+        else:
+            scale_delta = -0.1
+
+        new_scale = self.camera_sprites.scale + scale_delta
+        new_scale = min(new_scale, MAX_CAMERA_SCALE)
+        new_scale = max(new_scale, MIN_CAMERA_SCALE)
+
+        self.camera_sprites.scale = new_scale
 
 
 def main():
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
-import pytest
 import arcade
+import pytest
 from main import MyGame
 from pyglet.math import Vec2
+from pytest import approx
 
 
 @pytest.fixture(autouse=True)
@@ -9,17 +10,39 @@ def hide_game_window():
     MyGame.VISIBLE = False
 
 
-def test_camera_starts_at_origon():
-    game = MyGame()
+class TestCamera:
+    def test_camera_starts_at_origo(self):
+        game = MyGame()
 
-    assert game.camera_sprites.position == Vec2(0, 0)
+        assert game.camera_sprites.position == Vec2(0, 0)
 
+    def test_camera_pans_when_right_clicking_and_dragging(self):
+        game = MyGame()
 
-def test_camera_pans_when_right_clicking_and_dragging():
-    game = MyGame()
+        game.on_mouse_press(x=100, y=100, button=arcade.MOUSE_BUTTON_RIGHT, modifiers=0)
+        game.on_mouse_motion(x=200, y=300, dx=100, dy=200)
+        game.on_mouse_release(
+            x=200, y=300, button=arcade.MOUSE_BUTTON_RIGHT, modifiers=0
+        )
 
-    game.on_mouse_press(x=100, y=100, button=arcade.MOUSE_BUTTON_RIGHT, modifiers=0)
-    game.on_mouse_motion(x=200, y=300, dx=100, dy=200)
-    game.on_mouse_release(x=200, y=300, button=arcade.MOUSE_BUTTON_RIGHT, modifiers=0)
+        # TODO: forgot assert here
+        assert game.camera_sprites
 
-    assert game.camera_sprites
+    def test_camera_starts_with_scale_1(self):
+        game = MyGame()
+
+        assert game.camera_sprites.scale == 1.0
+
+    def test_scrolling_up_zooms_in(self):
+        game = MyGame()
+
+        game.on_mouse_scroll(x=100, y=100, scroll_x=0, scroll_y=1)
+
+        assert game.camera_sprites.scale == approx(0.9)
+
+    def test_scrolling_down_zooms_out(self):
+        game = MyGame()
+
+        game.on_mouse_scroll(x=100, y=100, scroll_x=0, scroll_y=-1)
+
+        assert game.camera_sprites.scale == approx(1.1)
