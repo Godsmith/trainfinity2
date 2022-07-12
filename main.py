6125072a23a012899ffdcb641c3a9203aaa056ab
Filip Lange <filip.lange@gmail.com>
2022-07-12 23:28:46 +0200

    Support building rails

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from collections import namedtuple
+from dataclasses import dataclass
 from re import S
+from typing import Iterable, Literal
 from pyglet.math import Vec2
 import arcade
+import arcade.color
+from arcade import Color
+import math
+from itertools import pairwise
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -10,6 +17,10 @@ GRID_HEIGHT = 600
 GRID_BOX_SIZE = 30
 GRID_LINE_WIDTH = 1
 GRID_COLOR = arcade.color.BLACK
+FINISHED_RAIL_COLOR = arcade.color.RED
+BUILDING_RAIL_COLOR = arcade.color.RED_BROWN
+RAIL_LINE_WIDTH = 5
+
 
 # Min zoom = 1/MAX_CAMERA_SCALE, i.e. 25%
 MAX_CAMERA_SCALE = 4
@@ -17,17 +28,62 @@ MAX_CAMERA_SCALE = 4
 MIN_CAMERA_SCALE = 0.5
 
 
+Rail = namedtuple("Rail", "x1 y1 x2 y2")
+
+
+class Grid:
+    def __init__(self) -> None:
+        self.rails_being_built = []
+        self.rails = []
+
+    def snap_to_x(self, x) -> int:
+        return math.floor(x / GRID_BOX_SIZE) * GRID_BOX_SIZE
+
+    def snap_to_y(self, y) -> int:
+        return math.floor(y / GRID_BOX_SIZE) * GRID_BOX_SIZE
+
+    def _is_not_straight_horizontal_or_diagonal(self, xs, ys):
+        return len(xs) != len(ys) and (
+            (len(xs) > 1 and len(ys) != 1) or (len(ys) > 1 and len(xs) != 1)
+        )
+
+    def click_and_drag(self, x, y, start_x, start_y):
+        x = self.snap_to_x(x)
+        y = self.snap_to_y(y)
+        start_x = self.snap_to_x(start_x)
+        start_y = self.snap_to_y(start_y)
+
+        dx = GRID_BOX_SIZE if x < start_x else -GRID_BOX_SIZE
+        dy = GRID_BOX_SIZE if y < start_y else -GRID_BOX_SIZE
+
+        xs = list(range(x, start_x + int(dx / abs(dx)), dx))
+        ys = list(range(y, start_y + int(dy / abs(dy)), dy))
+
+        if self._is_not_straight_horizontal_or_diagonal(xs, ys):
+            return
+
+        if len(xs) > len(ys):
+            ys = ys * len(xs)
+        elif len(ys) > len(xs):
+            xs = xs * len(ys)
+
+        self.rails_being_built = [
+            Rail(x1, y1, x2, y2) for (x1, y1), (x2, y2) in pairwise(zip(xs, ys))
+        ]
+
+    def release_mouse_button(self):
+        self.rails.extend(self.rails_being_built)
+        self.rails_being_built.clear()
+
+
 class MyGame(arcade.Window):
-
-    VISIBLE = True  # Overridden for unit testing
-
-    def __init__(self):
+    def __init__(self, visible=True):
         super().__init__(
             width=SCREEN_WIDTH,
             height=SCREEN_HEIGHT,
             title="TRAINFINITY",
-            visible=self.VISIBLE,
-        )
+            visible=visible,
+        )  # type: ignore
 
         self.horizontal_grid_lines = []
         self.vertical_grid_lines = []
@@ -37,10 +93,17 @@ class MyGame(arcade.Window):
         self.camera_sprites = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
         self.camera_gui = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
 
+        self.is_mouse1_pressed = False
+        self.mouse1_pressed_x = 0
+        self.mouse1_pressed_y = 0
+
         self.is_mouse2_pressed = False
         self.mouse2_pressed_x = 0
         self.mouse2_pressed_y = 0
+
         self.camera_position_when_mouse2_pressed = self.camera_sprites.position
+
+        self.grid = Grid()
 
     def setup(self):
         pass
@@ -52,12 +115,21 @@ class MyGame(arcade.Window):
         for y in range(0, GRID_HEIGHT + 1, GRID_BOX_SIZE):
             arcade.draw_line(0, y, GRID_WIDTH, y, GRID_COLOR, GRID_LINE_WIDTH)
 
+    def _draw_rails(self, rails: Iterable[Rail], color: Color):
+        for rail in rails:
+            x1, y1, x2, y2 = [coordinate + GRID_BOX_SIZE / 2 for coordinate in rail]
+            arcade.draw_line(x1, y1, x2, y2, color, RAIL_LINE_WIDTH)
+
+    def _draw_all_rails(self):
+        self._draw_rails(self.grid.rails_being_built, BUILDING_RAIL_COLOR)
+        self._draw_rails(self.grid.rails, FINISHED_RAIL_COLOR)
+
     def on_draw(self):
         self.clear()
 
         self.camera_sprites.use()
-
         self._draw_grid()
+        self._draw_all_rails()
 
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
         if button == arcade.MOUSE_BUTTON_RIGHT:
@@ -65,9 +137,17 @@ class MyGame(arcade.Window):
             self.mouse2_pressed_y = y
             self.is_mouse2_pressed = True
             self.camera_position_when_mouse2_pressed = self.camera_sprites.position
+        elif button == arcade.MOUSE_BUTTON_LEFT:
+            self.mouse1_pressed_x = x
+            self.mouse1_pressed_y = y
+            self.is_mouse1_pressed = True
 
     def on_mouse_release(self, x: int, y: int, button: int, modifiers: int):
-        self.is_mouse2_pressed = False
+        if button == arcade.MOUSE_BUTTON_RIGHT:
+            self.is_mouse2_pressed = False
+        elif button == arcade.MOUSE_BUTTON_LEFT:
+            self.is_mouse1_pressed = False
+            self.grid.release_mouse_button()
 
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
         if self.is_mouse2_pressed:
@@ -89,6 +169,8 @@ class MyGame(arcade.Window):
             new_position = Vec2(new_position.x, min(max_y, new_position.y))
 
             self.camera_sprites.move(new_position)
+        elif self.is_mouse1_pressed:
+            self.grid.click_and_drag(x, y, self.mouse1_pressed_x, self.mouse1_pressed_y)
 
     def on_mouse_scroll(self, x: int, y: int, scroll_x: int, scroll_y: int):
         if scroll_y < 0:
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,24 +1,21 @@
 import arcade
 import pytest
-from main import MyGame
+from main import MyGame, Rail
 from pyglet.math import Vec2
 from pytest import approx
 
 
 @pytest.fixture(autouse=True)
-def hide_game_window():
-    MyGame.VISIBLE = False
+def game() -> MyGame:
+    game = MyGame(visible=False)
+    return game
 
 
 class TestCamera:
-    def test_camera_starts_at_origo(self):
-        game = MyGame()
-
+    def test_camera_starts_at_origo(self, game):
         assert game.camera_sprites.position == Vec2(0, 0)
 
-    def test_camera_pans_when_right_clicking_and_dragging(self):
-        game = MyGame()
-
+    def test_camera_pans_when_right_clicking_and_dragging(self, game):
         game.on_mouse_press(x=100, y=100, button=arcade.MOUSE_BUTTON_RIGHT, modifiers=0)
         game.on_mouse_motion(x=200, y=300, dx=100, dy=200)
         game.on_mouse_release(
@@ -28,9 +25,7 @@ class TestCamera:
         # TODO: forgot assert here
         assert game.camera_sprites
 
-    def test_camera_stops_when_trying_to_move_past_top_left_corner(self):
-        game = MyGame()
-
+    def test_camera_stops_when_trying_to_move_past_top_left_corner(self, game):
         game.on_mouse_press(x=100, y=100, button=arcade.MOUSE_BUTTON_RIGHT, modifiers=0)
         game.on_mouse_motion(x=2000, y=3000, dx=1900, dy=2900)
         game.on_mouse_release(
@@ -39,9 +34,7 @@ class TestCamera:
 
         assert game.camera_sprites.goal_position == Vec2(-400, -300)
 
-    def test_camera_stops_when_trying_to_move_past_bottom_right_corner(self):
-        game = MyGame()
-
+    def test_camera_stops_when_trying_to_move_past_bottom_right_corner(self, game):
         game.on_mouse_press(x=100, y=100, button=arcade.MOUSE_BUTTON_RIGHT, modifiers=0)
         game.on_mouse_motion(x=-2000, y=-3000, dx=-2100, dy=-3100)
         game.on_mouse_release(
@@ -50,21 +43,51 @@ class TestCamera:
 
         assert game.camera_sprites.goal_position == Vec2(200, 300)
 
-    def test_camera_starts_with_scale_1(self):
-        game = MyGame()
-
+    def test_camera_starts_with_scale_1(self, game):
         assert game.camera_sprites.scale == 1.0
 
-    def test_scrolling_up_zooms_in(self):
-        game = MyGame()
-
+    def test_scrolling_up_zooms_in(self, game):
         game.on_mouse_scroll(x=100, y=100, scroll_x=0, scroll_y=1)
 
         assert game.camera_sprites.scale == approx(0.9)
 
-    def test_scrolling_down_zooms_out(self):
-        game = MyGame()
-
+    def test_scrolling_down_zooms_out(self, game):
         game.on_mouse_scroll(x=100, y=100, scroll_x=0, scroll_y=-1)
 
         assert game.camera_sprites.scale == approx(1.1)
+
+
+class TestGrid:
+    def test_horizontal_rail_being_built(self, game: MyGame):
+        game.on_mouse_press(x=90, y=90, button=arcade.MOUSE_BUTTON_LEFT, modifiers=0)
+        game.on_mouse_motion(x=120, y=90, dx=30, dy=0)
+
+        assert game.grid.rails_being_built == [Rail(120, 90, 90, 90)]
+
+    def test_vertical_rail_being_built(self, game: MyGame):
+        game.on_mouse_press(x=90, y=90, button=arcade.MOUSE_BUTTON_LEFT, modifiers=0)
+        game.on_mouse_motion(x=90, y=120, dx=0, dy=30)
+
+        assert game.grid.rails_being_built == [Rail(90, 120, 90, 90)]
+
+    def test_diagonal_rail_being_built(self, game: MyGame):
+        game.on_mouse_press(x=90, y=90, button=arcade.MOUSE_BUTTON_LEFT, modifiers=0)
+        game.on_mouse_motion(x=120, y=120, dx=30, dy=30)
+
+        assert game.grid.rails_being_built == [Rail(120, 120, 90, 90)]
+
+    def test_trying_to_build_non_straight_does_nothing(self, game: MyGame):
+        game.on_mouse_press(x=100, y=100, button=arcade.MOUSE_BUTTON_LEFT, modifiers=0)
+        game.on_mouse_motion(x=160, y=130, dx=60, dy=30)
+
+        assert len(game.grid.rails_being_built) == 0
+
+    def test_built_rail(self, game: MyGame):
+        game.on_mouse_press(x=100, y=100, button=arcade.MOUSE_BUTTON_LEFT, modifiers=0)
+        game.on_mouse_motion(x=130, y=100, dx=30, dy=0)
+        game.on_mouse_release(
+            x=130, y=100, button=arcade.MOUSE_BUTTON_LEFT, modifiers=0
+        )
+
+        assert len(game.grid.rails_being_built) == 0
+        assert len(game.grid.rails) == 1
