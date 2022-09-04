366e5e25ca74058d2ecdbeba12026d32f95cece9
Filip Lange <filip.lange@gmail.com>
2022-09-04 22:32:29 +0200

    Remove unused protocols

--- protocols.py
+++ protocols.py
@@ -1,23 +1,13 @@
 from typing import Protocol
 import typing
-from pyglet.math import Vec2
 
 if typing.TYPE_CHECKING:
-    from train import Train
-    from model import Rail, Signal
+    from model import Rail
 
 
 class GridEnlarger(Protocol):
     def enlarge_grid(self):
         raise NotImplementedError
-
-
-class TrainCollection(Protocol):
-    trains: list["Train"]
-
-
-class SignalCollection(Protocol):
-    signals: dict[Vec2, "Signal"]
 
 
 class RailCollection(Protocol):
