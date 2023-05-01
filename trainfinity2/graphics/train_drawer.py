30e13c410f15f8a06ec0dfcff30914caa6dd587d
Filip Lange <filip.lange@gmail.com>
2023-05-01 15:49:06 +0200

    Improve selected train visualization

--- trainfinity2/graphics/train_drawer.py
+++ trainfinity2/graphics/train_drawer.py
@@ -8,7 +8,6 @@ from trainfinity2.constants import (
     GRID_BOX_SIZE,
     HIGHLIGHT_COLOR,
     IRON_SIZE,
-    TRAIN_RADIUS,
 )
 from trainfinity2.train import Train
 from trainfinity2.wagon import Wagon
@@ -85,23 +84,23 @@ class TrainDrawer:
             tilt_angle=train.angle,
         )
         if train.selected:
-            arcade.draw_circle_outline(
-                x, y, TRAIN_RADIUS, color=HIGHLIGHT_COLOR, border_width=5
+            arcade.draw_rectangle_outline(
+                x,
+                y,
+                GRID_BOX_SIZE * 0.5,
+                GRID_BOX_SIZE * 0.8,
+                color=HIGHLIGHT_COLOR,
+                tilt_angle=-train.angle,  # Not sure why minus sign needed here
+                border_width=5,
             )
             if train.rails_on_route:
-                positions = {
-                    position
+                positions = [
+                    # (train.x + GRID_BOX_SIZE / 2, train.y + GRID_BOX_SIZE / 2)
+                    (position.x + GRID_BOX_SIZE / 2, position.y + GRID_BOX_SIZE / 2)
                     for rail in train.rails_on_route[1:]
                     for position in rail.positions
-                }
-                for position in positions:
-                    arcade.draw_rectangle_filled(
-                        position.x + GRID_BOX_SIZE / 2,
-                        position.y + GRID_BOX_SIZE / 2,
-                        GRID_BOX_SIZE,
-                        GRID_BOX_SIZE,
-                        color=HIGHLIGHT_COLOR,
-                    )
+                ]
+                arcade.draw_lines(positions, color=HIGHLIGHT_COLOR, line_width=4)
 
     def _draw_wagon(self, wagon: Wagon):
         x = wagon.x + GRID_BOX_SIZE / 2
