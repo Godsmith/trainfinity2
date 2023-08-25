93811ecac622798d8c9b7d8e60d24738f9ff799e
Filip Lange <filip.lange@gmail.com>
2023-08-25 22:57:25 +0200

    Fix formatting of pyi files and make black run on them

--- stubs/perlin_noise/rand_vec.pyi
+++ stubs/perlin_noise/rand_vec.pyi
@@ -1,5 +1,10 @@
 from _typeshed import Incomplete
-from perlin_noise.tools import dot as dot, fade as fade, product as product, sample_vector as sample_vector
+from perlin_noise.tools import (
+    dot as dot,
+    fade as fade,
+    product as product,
+    sample_vector as sample_vector,
+)
 from typing import List, Tuple
 
 class RandVec:
